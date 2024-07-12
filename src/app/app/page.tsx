5ca4fe564b42d608baa9963fff17ce46cdf835cc
brighamdent <brighamdent@gmail.com>
2024-07-12 11:54:56 -0600

    minor fixes

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import Image from "next/image";
 import background from "../../../public/main-photo.jpg";
@@ -12,10 +11,8 @@ import Budget from "src/components/Budget";
 import Income from "src/components/income/Income";
 import Expenses from "src/components/Expenses";
 import Savings from "src/components/Savings";
-import { useAuth } from "src/context/AuthContext";
 
 export default function page() {
-  const { currUserData } = useAuth();
   return (
     <div className="relative h-screen overflow-hidden">
       <div className="absolute inset-0 z-0">
@@ -32,7 +29,6 @@ export default function page() {
 
       <div className="relative z-10 h-full flex items-center justify-center">
         <div className=" pl-20 pr-20 p-2 h-16 items-center w-full bg-[#25264653] absolute top-0 flex justify-between">
-          <h1>{currUserData?.email}</h1>
           <h1 className="text-2xl">PennyArc</h1>
           <div className="flex justify-around items-center w-44">
             <h1 className="text-5xl">June</h1>
