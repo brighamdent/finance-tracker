319f64485e4ce87fa3383c7ebcc4c48684890b56
brighamdent <brighamdent@gmail.com>
2024-07-12 20:44:09 -0600

    small change

--- src/components/budget/EditBudgetModal.tsx
+++ src/components/budget/EditBudgetModal.tsx
@@ -1,6 +1,6 @@
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export function EditBudgetModal({
   item,
